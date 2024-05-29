0bad63cc23ba3b66edc7f6b7b929a04b50a84571
HDongyeop <whitesea890@gmail.com>
2024-05-29 16:53:04 +0900

    Vector2 함수 추가
    * 초기화
    * 길이
    * 정규화(길이1로)
    * 회전
    * 내적(점곱)
    * 외적(가위곱)

--- MegiEngine_SOURCE/CommonInclude.h
+++ MegiEngine_SOURCE/CommonInclude.h
@@ -10,6 +10,7 @@
 #include <map>
 #include <functional>
 #include <filesystem>
+#include <math.h>
 
 #include "MegiMath.h"
 
--- MegiEngine_SOURCE/MegiMath.h
+++ MegiEngine_SOURCE/MegiMath.h
@@ -3,6 +3,7 @@
 
 namespace MegiEngine::Math
 {
+#define PI 3.141592f
 	struct Vector2
 	{
 		static Vector2 Zero;
@@ -19,6 +20,47 @@ namespace MegiEngine::Math
 		Vector2(float _x , float _y) : x(_x) , y(_y)
 		{
 			
+		}
+
+		void Clear()
+		{
+			x = 0.0f;
+			y = 0.0f;
+		}
+
+		float Length()
+		{
+			return sqrtf(x * x + y * y);
+		}
+
+		Vector2 Normalize()
+		{
+			float len = Length();
+			x /= len;
+			y /= len;
+
+			return *this;
+		}
+
+		Vector2 Rotate(Vector2 vector, float degree)
+		{
+			float radian = ( degree / 180.0f ) * PI;
+			vector.Normalize();
+
+			float x = cosf(radian) * vector.x - sinf(radian) * vector.y;
+			float y = sinf(radian) * vector.x + cosf(radian) * vector.y;
+
+			return Vector2(x , y);
+		}
+
+		float Dot(Vector2& v1, Vector2& v2)
+		{
+			return v1.x * v2.x + v1.y * v2.y;
+		}
+
+		float Cross(Vector2 v1, Vector2 v2)
+		{
+			return v1.x * v2.y - v1.y * v2.x;
 		}
 
 		Vector2 operator+(const Vector2& other) const
